32f83d5c626dbf9146cd32bcd689ffa5633d0517
Mishra <r78mishra@gmail.com>
2023-10-02 18:48:26 +0530

    Duplicate records found committed

--- Practice-java-project/src/com/demo/duplicate_fromlist.java
+++ Practice-java-project/src/com/demo/duplicate_fromlist.java
@@ -1,5 +1,11 @@
 package com.demo;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class duplicate_fromlist {
 
 	public static void main(String[] args) {
@@ -11,7 +17,10 @@ public class duplicate_fromlist {
 	{
 		System.out.println(string.toUpperCase()); 
 	}
-
+	List<Integer> list=Arrays.asList(1,2,3,4,5,6,7,8,9,3,3,3,4,5,6,4,5);
+	Set<Integer> duplicate=list.stream().filter(p->Collections.frequency(list, p)>1).
+			collect(Collectors.toSet());
+	System.out.println("Duplicat Records Are\t"+duplicate); 
 	}
 
 }
