7d3c39b660ea2de7a1b76f9ad6179d3aca8bf8f9
Mishra <r78mishra@gmail.com>
2023-10-02 17:44:26 +0530

    New sysout is used

--- Practice-java-project/src/com/demo/duplicate_fromlist.java
+++ Practice-java-project/src/com/demo/duplicate_fromlist.java
@@ -4,6 +4,10 @@ public class duplicate_fromlist {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
 
 	}
 
