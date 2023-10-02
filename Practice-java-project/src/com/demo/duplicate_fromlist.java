a0f3ef5a01b82d31dbc1470d7c10b20cad5ef5a1
Mishra <r78mishra@gmail.com>
2023-10-02 18:36:08 +0530

    for loop regarding to string uppercase update

--- Practice-java-project/src/com/demo/duplicate_fromlist.java
+++ Practice-java-project/src/com/demo/duplicate_fromlist.java
@@ -6,6 +6,11 @@ public class duplicate_fromlist {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
 		System.out.println("Hello");
+	String string="amazon";
+	for(int i=0;i<string.length();i++)
+	{
+		System.out.println(string.toUpperCase()); 
+	}
 
 	}
 
